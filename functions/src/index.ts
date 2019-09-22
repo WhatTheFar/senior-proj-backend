32dfdb3bd0226f1f5cd524edaced608d7313852e
WhatTheFar <jakpat.m@gmail.com>
2019-09-22 15:18:19 +0700

    feat(ping): init ping http function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,10 @@
 import * as functions from 'firebase-functions';
 import MicroGear = require('microgear');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions
+export const ping = functions
 	.region('asia-east2')
 	.https.onRequest((request, response) => {
-		response.send('Hello from Firebase!');
+		response.send('pong');
 	});
 
 function connectNetpie(): Promise<any> {
