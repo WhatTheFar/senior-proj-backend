772dc9dcc24ec5581b4f1d1b78af0b222d705b21
WhatTheFar <jakpat.m@gmail.com>
2019-09-23 11:58:15 +0700

    feat(config): init config module

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IotModule } from './iot/iot.module';
+import { MicroGearModule } from './micro-gear/micro-gear.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [IotModule, MicroGearModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nest/src/config/config.service.ts
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv';
+
+function getEnv(key: string): string | null {
+  const value = process.env[key];
+  if (value) {
+    return value;
+  } else {
+    return null;
+  }
+}
+
+function getEnvOrThrows(key: string): string {
+  const value = getEnv(key);
+  if (value) {
+    return value;
+  } else {
+    throw new Error(`Unable to read "${key}" environment`);
+  }
+}
+
+export class ConfigService {
+  constructor() {
+    dotenv.config();
+  }
+
+  get(key: string): string {
+    return getEnv(key);
+  }
+  getDatabaseHostName(): string {
+    return getEnvOrThrows('DB_HOST');
+  }
+  getDatabaseUser(): string {
+    return getEnvOrThrows('DB_USER');
+  }
+  getDatabasePassword(): string {
+    return getEnvOrThrows('DB_PASSWORD');
+  }
+}
