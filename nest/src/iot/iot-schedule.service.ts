48b0ef05fb096bf0642d0e924df508d5a1d5e5f7
WhatTheFar <jakpat.m@gmail.com>
2019-09-25 15:32:40 +0700

    refactor(iot): schedule jobs dynamically
    schedule jobs dynamically on application bootstrap

--- nest/src/iot/iot-schedule.service.ts
+++ nest/src/iot/iot-schedule.service.ts
@@ -1,19 +1,45 @@
 import { IotService } from './iot.service';
-import { Injectable } from '@nestjs/common';
-import { Cron, NestSchedule } from 'nest-schedule';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { Cron, NestSchedule, Schedule, InjectSchedule } from 'nest-schedule';
+
+// @Injectable() // Only support SINGLETON scope
+// export class IotScheduleService extends NestSchedule {
+//   constructor(private readonly iotService: IotService) {
+//     super();
+//   }
+
+//   @Cron('*/1 * * * *')
+//   async iotSensorSyncronizationCronJob() {
+//     const date = new Date();
+//     console.log(
+//       `${date.toISOString()} : IoT Syncronization, this will be run every 1 minute!`,
+//     );
+//     this.iotService.publishTimeSyncronization(date);
+//   }
+// }
 
 @Injectable() // Only support SINGLETON scope
-export class IotScheduleService extends NestSchedule {
-  constructor(private readonly iotService: IotService) {
-    super();
+export class IotScheduleService implements OnApplicationBootstrap {
+  constructor(
+    @InjectSchedule() private readonly schedule: Schedule,
+    private readonly iotService: IotService,
+  ) {}
+
+  onApplicationBootstrap() {
+    this.schedule.scheduleCronJob(
+      'iot',
+      // '*/10 * * * * *',
+      '*/1 * * * *',
+      this.iotSensorSyncronizationCronJob.bind(this),
+    );
   }
 
-  @Cron('*/1 * * * *')
-  async iotSensorSyncronizationCronJob() {
+  async iotSensorSyncronizationCronJob(): Promise<boolean> {
     const date = new Date();
     console.log(
       `${date.toISOString()} : IoT Syncronization, this will be run every 1 minute!`,
     );
     this.iotService.publishTimeSyncronization(date);
+    return false;
   }
 }
