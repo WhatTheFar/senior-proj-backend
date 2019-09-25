0516261b8460aaaa73d3b255c8f608f36d729440
WhatTheFar <jakpat.m@gmail.com>
2019-09-25 14:46:42 +0700

    feat(iot): complete saveCO2 saveMultiSensors svc

--- nest/src/iot/iot.service.spec.ts
+++ nest/src/iot/iot.service.spec.ts
@@ -1,20 +1,188 @@
+import { IIot } from './../../dist/iot/iot.model.d';
+import { ConfigService } from './../config/config.service';
+import { IOT_MODEL, IotSchema } from './iot.model';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { MicroGearModule } from './../microgear/microgear.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { IotService } from './iot.service';
+import { Model } from 'mongoose';
+import { ObjectID } from 'bson';
 
 describe('IotService', () => {
   let service: IotService;
+  let iotModel: Model<IIot>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [MicroGearModule],
+      imports: [
+        MongooseModule.forRootAsync({
+          useClass: ConfigService,
+        }),
+        MongooseModule.forFeature([
+          { name: IOT_MODEL, schema: IotSchema, collection: 'sensors' },
+        ]),
+        MicroGearModule,
+      ],
       providers: [IotService],
     }).compile();
 
     service = module.get<IotService>(IotService);
+    iotModel = module.get<Model<IIot>>(getModelToken(IOT_MODEL));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('saveCO2', () => {
+    describe('Given an empty DB', () => {
+      beforeEach(async () => {
+        await iotModel.deleteMany({});
+      });
+
+      describe('saveCO2 called', () => {
+        const date = new Date();
+        const co2 = 123.456;
+        beforeEach(async () => {
+          await service.saveCO2(date, co2);
+        });
+
+        it('document should be created in DB', async () => {
+          const docs = await iotModel.find({ date });
+
+          expect(docs.length).toEqual(1);
+          expect(docs[0].toObject()).toEqual({
+            __v: expect.any(Number),
+            _id: expect.any(ObjectID),
+            date,
+            co2,
+            multi: [],
+          });
+          expect(docs[0].multi.length).toEqual(0);
+        });
+      });
+    });
+
+    describe('Given multi sensors has been saved', () => {
+      const date = new Date();
+      const co2 = 123.456;
+
+      const device = 1;
+      const temp = 42.5;
+      const hum = 42.5;
+      const light = 100.2;
+      beforeEach(async () => {
+        await iotModel.deleteMany({});
+        await service.saveMultiSensors(date, { device, temp, hum, light });
+      });
+
+      describe('saveCO2 called on the same date', () => {
+        beforeEach(async () => {
+          await service.saveCO2(date, co2);
+        });
+
+        it('document should be modified', async () => {
+          const docs = await iotModel.find({ date });
+
+          expect(docs.length).toEqual(1);
+          expect(docs[0].toObject()).toEqual({
+            __v: expect.any(Number),
+            _id: expect.any(ObjectID),
+            date,
+            co2,
+            multi: [
+              {
+                _id: expect.any(ObjectID),
+                device,
+                temp,
+                hum,
+                light,
+              },
+            ],
+          });
+        });
+      });
+    });
+  });
+
+  describe('saveMultiSensors', () => {
+    describe('Given an empty DB', () => {
+      beforeEach(async () => {
+        await iotModel.deleteMany({});
+      });
+
+      describe('saveMultiSensors called', () => {
+        const date = new Date();
+        const device = 1;
+        const temp = 42.5;
+        const hum = 42.5;
+        const light = 100.2;
+        beforeEach(async () => {
+          await service.saveMultiSensors(date, { device, temp, hum, light });
+        });
+
+        it('document should be created in DB', async () => {
+          const docs = await iotModel.find({ date });
+
+          expect(docs.length).toEqual(1);
+          expect(docs[0].toObject()).toEqual({
+            __v: expect.any(Number),
+            _id: expect.any(ObjectID),
+            date,
+            multi: [
+              {
+                _id: expect.any(ObjectID),
+                device,
+                temp,
+                hum,
+                light,
+              },
+            ],
+          });
+          expect(docs[0].multi.length).toEqual(1);
+        });
+      });
+    });
+
+    describe('Given co2 sensors has been saved', () => {
+      const date = new Date();
+      const co2 = 123.456;
+
+      const device = 1;
+      const temp = 42.5;
+      const hum = 42.5;
+      const light = 100.2;
+      beforeEach(async () => {
+        await iotModel.deleteMany({});
+        await service.saveCO2(date, co2);
+      });
+
+      describe('saveCO2 called on the same date', () => {
+        beforeEach(async () => {
+          await service.saveMultiSensors(date, { device, temp, hum, light });
+        });
+
+        it('document should be modified', async () => {
+          const docs = await iotModel.find({ date });
+
+          expect(docs.length).toEqual(1);
+          expect(docs[0].toObject()).toEqual({
+            __v: expect.any(Number),
+            _id: expect.any(ObjectID),
+            date,
+            co2,
+            multi: [
+              {
+                _id: expect.any(ObjectID),
+                device,
+                temp,
+                hum,
+                light,
+              },
+            ],
+          });
+        });
+      });
+    });
   });
 });
--- nest/src/iot/iot.service.ts
+++ nest/src/iot/iot.service.ts
@@ -1,5 +1,8 @@
 import { MicroGearService } from './../microgear/microgear.service';
 import { Injectable } from '@nestjs/common';
+import { IOT_MODEL, IIot, MultiSensor } from './iot.model';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 /**
  * Constants for NETPIE
@@ -8,9 +11,38 @@ const NETPIE_IOT_SYNC_TOPIC = '/iot';
 
 @Injectable()
 export class IotService {
-  constructor(private readonly microgearService: MicroGearService) {}
+  constructor(
+    private readonly microgearService: MicroGearService,
+    @InjectModel(IOT_MODEL) private readonly iotModel: Model<IIot>,
+  ) {}
 
   publishTimeSyncronization(time: Date) {
     this.microgearService.publish(NETPIE_IOT_SYNC_TOPIC, time.toISOString());
+  }
+
+  async saveCO2(date: Date, co2: number) {
+    const doc = await this.iotModel.findOne({
+      date,
+    });
+    if (doc) {
+      doc.co2 = co2;
+      await doc.save();
+    } else {
+      const newDoc = new this.iotModel({ date, co2 });
+      await newDoc.save();
+    }
+  }
+
+  async saveMultiSensors(date: Date, multi: MultiSensor) {
+    const doc = await this.iotModel.findOne({
+      date,
+    });
+    if (doc) {
+      doc.multi.push(multi);
+      await doc.save();
+    } else {
+      const newDoc = new this.iotModel({ date, multi: [multi] });
+      await newDoc.save();
+    }
   }
 }
