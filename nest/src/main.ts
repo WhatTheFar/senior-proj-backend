335422b3a5721392bd12f4dc998b7cfb67d00301
WhatTheFar <jakpat.m@gmail.com>
2019-10-28 10:56:35 +0700

    feat(swagger): get schemes from NODE_ENV

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -3,6 +3,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+function getSwaggerSchemes(): Array<'http' | 'https'> {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return ['http'];
+    case 'production':
+      return ['https'];
+    default:
+      return ['http'];
+  }
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
@@ -11,7 +22,7 @@ async function bootstrap() {
     .setTitle('Senior Project')
     .setDescription('The seniorproj API description')
     .setVersion('1.0')
-    .setSchemes('https', 'http')
+    .setSchemes(...getSwaggerSchemes())
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
