95d485bdd7d21f566d27c76bc2a2d78a8e8ea824
Jatuwatsag <thehandsomeify@gmail.com>
2019-10-30 15:21:32 +0700

    fix(dashboard): change interface

--- dashboard/src/types/sensor.ts
+++ dashboard/src/types/sensor.ts
@@ -1,10 +1,29 @@
-export interface SensorInfo {
-  date: Date;
-  people: {
-    people: number;
-  };
+export interface People {
+  actualDate: Date;
+  people: number;
+}
+
+export interface Co2 {
+  _id: string;
+  actualDate: Date;
+  device: number;
   co2: number;
-  hum: number;
+}
+
+export interface Multi {
+  _id: string;
+  actualDate: Date;
+  device: number;
   temp: number;
+  hum: number;
   light: number;
+}
+
+export interface SensorInfo {
+  _id: string;
+  date: Date;
+  people: People;
+  co2: Co2[];
+  multi: Multi[];
+  __v: number;
 }
