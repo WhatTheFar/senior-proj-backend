f92a6373c7b152c6c2347b43d2b6c63288eb3b03
Jatuwatsag <thehandsomeify@gmail.com>
2019-10-30 19:00:05 +0700

    feat(dashboard): create vuex component

--- dashboard/src/store/index.ts
+++ dashboard/src/store/index.ts
@@ -1,15 +1,59 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
+import { SensorInfo } from "@/types/sensor";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+interface Store {
+  sensorInfo: SensorInfo[];
+  currentInterval: number;
+}
+
+export default new Vuex.Store<Store>({
   state: {
+    sensorInfo: [],
+    currentInterval: -1
+  },
+  getters: {
+    sensorInfo: ({ sensorInfo }) => sensorInfo
+    /*
+      sensorInfo: (state) => {
+        return state.sensorInfo,
+      }
+     */
   },
   mutations: {
+    setSensorInfo: (state, payload) => {
+      state.sensorInfo = payload;
+    },
+    setInterval: (state, payload) => {
+      state.currentInterval = payload;
+    }
   },
   actions: {
+    startPollingSensorInfo: async ({ commit }) => {
+      const { data } = await Vue.axios.get<SensorInfo>("iot/sensor", {
+        params: {
+          offset: 0,
+          limit: 10
+        }
+      });
+      commit("setSensorInfo", data);
+      const interval = setInterval(async () => {
+        const { data } = await Vue.axios.get<SensorInfo>("iot/sensor", {
+          params: {
+            offset: 0,
+            limit: 10
+          }
+        });
+        commit("setSensorInfo", data);
+      }, 30 * 1000);
+      commit("setInterval", interval);
+    },
+    stopPollingSensorInfo: ({ state, commit }) => {
+      clearInterval(state.currentInterval);
+      commit("setInterval", -1);
+    }
   },
-  modules: {
-  },
+  modules: {}
 });
