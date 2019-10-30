c56f5a49d33a0fb6120af09f3062e87716a289fd
WhatTheFar <jakpat.m@gmail.com>
2019-10-31 00:14:05 +0700

    feat(swagger): change swagger url to `/api-doc`

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .setBasePath(BASE_PREFIX)
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-doc', app, document);
 
   await app.listen(3000);
 }
