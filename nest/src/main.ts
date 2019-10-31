08d61756b5de9f39c79dfcc599838c068b632208
WhatTheFar <jakpat.m@gmail.com>
2019-10-31 10:11:49 +0700

    fix(nest): remove global prefix path
    remove global prefix path for iot devices backward compatible

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -14,19 +14,15 @@ function getSwaggerSchemes(): Array<'http' | 'https'> {
   }
 }
 
-const BASE_PREFIX = 'api';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix(BASE_PREFIX);
 
   const options = new DocumentBuilder()
     .setTitle('Senior Project')
     .setDescription('The seniorproj API description')
     .setVersion('1.0')
     .setSchemes(...getSwaggerSchemes())
-    .setBasePath(BASE_PREFIX)
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-doc', app, document);
