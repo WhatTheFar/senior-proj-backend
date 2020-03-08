95eefcb6d12620f94b7cfa4d51dde47af3bd356b
WhatTheFar <jakpat.m@gmail.com>
2020-03-09 06:13:55 +0700

    feat(log): change netpie log collection name to 'netpie-logs'

--- packages/log-processing/src/model/netpie/netpie-log.model.ts
+++ packages/log-processing/src/model/netpie/netpie-log.model.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 import * as mongodb from 'mongodb';
 import { strEnum } from '../../string-enum';
 
-export const NETPIE_LOG_COLLECTION = 'netpie-log';
+export const NETPIE_LOG_COLLECTION = 'netpie-logs';
 
 /** Create a K:V */
 export const NetpieLogType = strEnum(['RESET_BG', 'SET_PEOPLE']);
